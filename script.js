d2ded30f7deb635d6346fb89f4caf1de755afbc4
Phonemz <phonemyintzaw3033@gmail.com>
2022-06-02 16:23:39 +0630

    day 01 commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// This is a comment - alert('hello, world')
+
+let age = 25
+let year = 2022
+
+console.log(age, year)
+
+age = 30
+console.log(age)
+
+const mark = 100
+console.log(mark)
+
+// this is a comment - mark = 200
+
+var score = 100
+console.log (score)
