8d4bef6b0c9700e2678cf271e1e5609447bdc4f2
Phonemz <phonemyintzaw3033@gmail.com>
2022-06-03 22:48:14 +0630

    day02 commit

---
+++ day02.js
@@ -0,0 +1,24 @@
+//String
+let word = 'hello,world'
+
+//String Concatenation
+let firstName = 'Hello'
+let lastName = 'World'
+
+let fullName = firstName + ' ' + lastName
+console.log(fullName)
+
+//getting characters
+console.log(fullName[0])
+
+//strings length
+console.log(fullName.length)
+let findLength = fullName.length
+console.log(findLength)
+
+//string method
+console.log(fullName.toUpperCase)
+console.log(fullName.toLowerCase)
+
+let index = word.indexOf('o')
+console.log(index)
