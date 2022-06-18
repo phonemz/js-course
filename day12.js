d820558a6e606370ae8faefdcf33ba7ce575500c
Phonemz <phonemyintzaw3033@gmail.com>
2022-06-18 14:47:59 +0630

    day 12 done

--- day12.js
+++ day12.js
@@ -1,12 +1,32 @@
 const form = document.querySelector('.signup-form')
+const feedback = document.querySelector('.feedback')
 
 form.addEventListener('submit', e => {
-    e.preventDefault()
-    console.log(form.username.value)
+    e.preventDefault();
+    const submitName = form.username.value
+    const pattern = /^[a-zA-Z0-9]{6,15}$/
+    if (pattern.test(submitName)) {
+        feedback.textContent = 'Username is Valid'
+    }
+    else {
+        feedback.textContent = 'Please use characters between a-z and 0-9 and 6 to 15 characters only'
+    }
 })
 
+form.username.addEventListener('keyup', e => {
+    if (pattern.test(e.target.value)) {
+        e.target.style.color = 'green' }
+    else {
+        e.target.style.color = 'red'
+    }
+})
+
+
+
+//Regular Expression - Regex
+
 const name = 'steven'
-const pattern = /^[a-zA-Z0-9]{6,}/
+const pattern = /^[a-zA-Z0-9]{6,15}/
 
 const result = pattern.test(name)
 console.log(result)
