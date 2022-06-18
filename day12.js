fb8b8485d8c446653b149972d7e55d171d34acc8
Phonemz <phonemyintzaw3033@gmail.com>
2022-06-18 14:12:00 +0630

    regular expression done

--- day12.js
+++ day12.js
@@ -4,3 +4,9 @@ form.addEventListener('submit', e => {
     e.preventDefault()
     console.log(form.username.value)
 })
+
+const name = 'steven'
+const pattern = /^[a-zA-Z0-9]{6,}/
+
+const result = pattern.test(name)
+console.log(result)
