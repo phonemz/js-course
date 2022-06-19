0dc2b5547d246910d2fdeebee76b2c61d5f80199
Phonemz <phonemyintzaw3033@gmail.com>
2022-06-19 22:05:14 +0630

    day 14 show the score

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('.quiz-form')
-const correctAnswer = ['right','right','right','right']
+const correctAnswer = ['right', 'right', 'right', 'right']
+const result = document.querySelector('.result')
 
 form.addEventListener('submit', e => {
     e.preventDefault()
@@ -16,6 +17,6 @@ form.addEventListener('submit', e => {
         
     })
 
-    console.log(score)
+    result.textContent = `Your score is ${score}`
    
 })
