1ece081fba67783fe147ca7910d60172cbe15d9b
Phonemz <phonemyintzaw3033@gmail.com>
2022-06-19 22:00:10 +0630

    day 13 project

---
+++ app.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('.quiz-form')
+const correctAnswer = ['right','right','right','right']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const userAnswer = [form.q1.value, form.q2.value, form.q3.value, form.q4.value]
+
+    let score = 0
+
+    userAnswer.forEach((answer, index) => {
+        
+        if (answer === correctAnswer[index]) {
+            score += 25
+        }
+
+        
+    })
+
+    console.log(score)
+   
+})
