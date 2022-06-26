419f58d755ced9bd57b6c7f1a2fd2ca4919c5ff4
Phonemz <phonemyintzaw3033@gmail.com>
2022-06-26 22:54:22 +0630

    day 19

---
+++ js/day19.js
@@ -0,0 +1,12 @@
+const now = new Date()
+
+console.log(now)
+console.log(typeof now)
+
+console.log(now.getFullYear())
+console.log(now.getMonth())
+console.log(now.getUTCFullYear())
+console.log(now.getTime())
+console.log(now.toDateString())
+console.log(now.toTimeString())
+console.log(now.getMilliseconds())
