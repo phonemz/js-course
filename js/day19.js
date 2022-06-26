b15e92086f173bc4c2f59881c5cb9648213859ec
Phonemz <phonemyintzaw3033@gmail.com>
2022-06-26 23:08:24 +0630

    timestamp and comparison

--- js/day19.js
+++ js/day19.js
@@ -10,3 +10,20 @@ console.log(now.getTime())
 console.log(now.toDateString())
 console.log(now.toTimeString())
 console.log(now.getMilliseconds())
+
+//TImestamps and comparison
+const before = new Date('February 1 2021 12:00:00')
+
+console.log(before.getTime())
+
+const diff = now.getTime() - before.getTime()
+
+console.log(diff)
+
+const mins = Math.round(diff / 1000 / 60)
+const hours = Math.round(mins / 60)
+const days = Math.round(hours / 24)
+const month = Math.round(days / 30)
+const year = Math.floor(month / 12)
+
+
