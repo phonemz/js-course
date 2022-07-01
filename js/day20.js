a3b37129a56cfc9600ea2bafd32e0dc2fd421753
Phonemz <phonemyintzaw3033@gmail.com>
2022-07-01 16:45:07 +0630

    day 20 done

---
+++ js/day20.js
@@ -0,0 +1,24 @@
+console.log(1)
+console.log(2)
+
+setTimeout(() => {
+    console.log('This is a async js function')
+}, 2000);
+console.log(3)
+console.log(4)
+
+const request = new XMLHttpRequest()
+
+
+
+request.addEventListener('readystatechange', () => {
+    if (request.readyState === 4 && request.status === 200) {
+        console.log( request.responseText)
+    }
+    else if (request.readyState === 4) {
+        console.log('could not fetch the data')
+    }
+})
+
+request.open('GET', 'https://jsonplaceholder.cypress.io/todos/')
+request.send()
