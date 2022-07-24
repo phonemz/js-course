4b4b071ea9757da9055273c0a5b07444ff5cab01
Phonemz <phonemyintzaw3033@gmail.com>
2022-07-24 22:57:15 +0630

    day 22

--- js/day20.js
+++ js/day20.js
@@ -7,21 +7,21 @@ setTimeout(() => {
 console.log(3)
 console.log(4)
 
-const request = new XMLHttpRequest()
+// const request = new XMLHttpRequest()
 
 
 
-request.addEventListener('readystatechange', () => {
-    if (request.readyState === 4 && request.status === 200) {
-        console.log( request.responseText)
-    }
-    else if (request.readyState === 4) {
-        console.log('could not fetch the data')
-    }
-    else {
-        console.log('Something went wrong')
-    }
-})
+// request.addEventListener('readystatechange', () => {
+//     if (request.readyState === 4 && request.status === 200) {
+//         console.log( request.responseText)
+//     }
+//     else if (request.readyState === 4) {
+//         console.log('could not fetch the data')
+//     }
+//     else {
+//         console.log('Something went wrong')
+//     }
+// })
 
-request.open('GET', 'https://jsonplaceholder.cypress.io/todos/')
-request.send()
+// request.open('GET', 'https://jsonplaceholder.cypress.io/todos/')
+// request.send();;/
--- js/day21.js
+++ js/day21.js
@@ -1,17 +1,19 @@
 console.log(1)
 console.log(2)
 
-const getTodos = (resource, callback) => {
+const getTodos = (resource) => {
+
+    return new Promise((resolve, reject) => {
     const request = new XMLHttpRequest()
 
     request.addEventListener('readystatechange', () => {
         
         if (request.readyState === 4 && request.status === 200) {
             const jsData = JSON.parse(request.responseText)
-            callback('error', jsData)
+            resolve ('this is resolved:' ,jsData)
         }
         else if (request.readyState === 4) {
-            callback('this is error','this is data')
+            reject ('this is rejected')
         }
         // else {
         //     callback('hello','world')
@@ -20,11 +22,18 @@ const getTodos = (resource, callback) => {
     
     request.open('GET', resource)
     request.send()
+})
+    
+    
 }
 
-// getTodos('js/data.json',(err, data) => {
-//     console.log(err, data)
-// })
+getTodos('js/data.json').then(data => {
+    console.log('hello this is resolved', data)
+}).catch(error => {
+    console.log('sorry this is rejected', error)
+})
+
+
 
 
 console.log(3)
