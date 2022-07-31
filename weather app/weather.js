5b4ea2650792d03d142e2abe628c67645f7805eb
Phonemz <phonemyintzaw3033@gmail.com>
2022-07-31 23:58:53 +0630

    city search api

--- weather app/weather.js
+++ weather app/weather.js
@@ -1,3 +1,15 @@
 const key = 'w4fI6V8vrzna8vfaOGaZT0lWXFqRS7J8'
 
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const query = `?apikey=${key}&q=${city}`
 
+    const response = await fetch(base + query)
+    const data = await response.json()
+
+    return data[0]
+}
+
+getCity('manchester')
+    .then(data => console.log(data))
+    .catch(error => console.log('error'))
