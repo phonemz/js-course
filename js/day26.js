6b54271556df1d04bf2aa04a023ed6cbd8a43304
Phonemz <phonemyintzaw3033@gmail.com>
2022-08-04 20:14:09 +0630

    day 26

---
+++ js/day26.js
@@ -0,0 +1,11 @@
+localStorage.setItem('name', 'pedri')
+localStorage.setItem('age', 20)
+
+// let name = localStorage.getItem('name')
+let age = localStorage.getItem('age')
+// console.log(name, age, typeof age)
+
+localStorage.setItem('name', 'gavi')
+let name = localStorage.getItem('name')
+console.log(name)
+
